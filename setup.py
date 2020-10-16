fbaf10ae8183dd0de42eca05ea6a4a594bc16de5
barrier <nicolas.barrier@ird.fr>
2020-10-16 17:03:26 +0200

    Correct forgotten long description (markdown)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     version="1.0.1",
     author="Nicolas Barrier",
     author_email="nicolas.barrier@ird.fr",
-    description=("Python package dedicated to the Earth Scientists. It contains a module for the processing of time-series (filtering, anomalies), a module for spatial analysis, a module with color-specific functions, a module for NetCDF processing and a module for spectral analysis"),
+    description=("Python package dedicated to the Earth Scientists."),
     long_description_content_type="text/markdown",
     packages=find_packages(),
     url="https://github.com/barriern/Environmental-toolkit",
@@ -20,6 +20,8 @@ setup(
               'mpl_toolkits.basemap',
               'datetime.datetime',
               'spectrum'],
+
+    long_description=long_description,
 
     classifiers = [
         #"Development Status :: 5 - Production/Stable",
