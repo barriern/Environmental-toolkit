fd4203be3335e1bd64a1cb5c92ea2dfedcc3b4cc
barrier <nicolas.barrier@ird.fr>
2020-10-16 16:41:31 +0200

    Update release version + gitignore

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@ with open("README.md", "r") as fh:
 
 setup(
     name="envtoolkit",
-    version="1.0.1.90",
+    version="1.0.1",
     author="Nicolas Barrier",
     author_email="nicolas.barrier@ird.fr",
     description=("Python package dedicated to the Earth Scientists. It contains a module for the processing of time-series (filtering, anomalies), a module for spatial analysis, a module with color-specific functions, a module for NetCDF processing and a module for spectral analysis"),
     long_description_content_type="text/markdown",
     packages=find_packages(),
+    url="https://github.com/barriern/Environmental-toolkit",
     requires=['numpy',
               'netCDF4',
               'matplotlib',
@@ -19,8 +20,6 @@ setup(
               'mpl_toolkits.basemap',
               'datetime.datetime',
               'spectrum'],
-    )
-
 
     classifiers = [
         #"Development Status :: 5 - Production/Stable",
@@ -37,4 +36,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
     ],
+
+)
+
            
